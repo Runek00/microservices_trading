a06c209092879d76ec20f10a7fd10c820180bf68
Runek <Runek00@gmail.com>
2021-02-19 00:34:44 +0100

    minor style fix

--- supplier_service/test/api/test_db_manager.py
+++ supplier_service/test/api/test_db_manager.py
@@ -10,13 +10,13 @@ class TestDbManagerSave(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         dbm.database.fetch_all = AsyncMock(return_value=['t1', 't2', 't3'])
 
+    @patch('app.api.db.database.execute_many',
+           new=AsyncMock(return_value=None))
     async def testSave(self):
-        with patch('app.api.db.database.execute_many',
-                   new=AsyncMock(return_value=None)) as _:
-            dict_correct = {
-                "name": "tau",
-                "price": 6.28,
-                "time": datetime(2013, 12, 11, 23, 22, 21)
-            }
-            result = await dbm.save_data([PriceIn(**dict_correct)])
+        dict_correct = {
+            "name": "tau",
+            "price": 6.28,
+            "time": datetime(2013, 12, 11, 23, 22, 21)
+        }
+        result = await dbm.save_data([PriceIn(**dict_correct)])
         self.assertEqual(result, None)
