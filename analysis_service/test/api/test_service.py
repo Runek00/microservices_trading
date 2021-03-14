85fa09a67d697071cd9160d8d02e598839a6a213
Paweł Runowski <prunowski@kartgis.com.pl>
2021-03-14 13:36:25 +0100

    service tests finished

--- analysis_service/test/api/test_service.py
+++ analysis_service/test/api/test_service.py
@@ -9,12 +9,6 @@ class TestService(unittest.IsolatedAsyncioTestCase):
     async def testGetData(self):
         respMock = Mock(spec=httpx.Response)
         respMock.status_code = 200
-        respMock.content = b'''
-        [
-            {"tc1": 1, "tc2": "one"},
-            {"tc1": 2, "tc2": "two"}
-        ]
-        '''
         respMock.json = lambda: [
             {"tc1": 1, "tc2": "one"},
             {"tc1": 2, "tc2": "two"}
@@ -23,4 +17,14 @@ class TestService(unittest.IsolatedAsyncioTestCase):
                    new=AsyncMock(return_value=respMock)) as test_post:
             df = await service.get_data()
             self.assertIsNotNone(df)
+            test_post.assert_called_once()
+
+    async def testSendBuy(self):
+        respMock = Mock(spec=httpx.Response)
+        respMock.status_code = 200
+        with patch('httpx._api.request',
+                   new=AsyncMock(return_value=respMock)) as test_post:
+            self.assertRaises(TypeError, service.send_buy)
+            df = await service.send_buy(['a', 'b', 'c'])
+            self.assertEqual(df, True)
             test_post.assert_called_once()
